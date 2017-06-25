4648621dc161d6c8054724944bbd2433c2ea4c4e
alexkatz <alexanderikatz@gmail.com>
2017-06-25 09:18:28 -0400

    updated build target to iOS 10, removed storyboard

--- Collisions/GameViewController.swift
+++ Collisions/GameViewController.swift
@@ -28,43 +28,39 @@ extension SKNode {
 
 class GameViewController: UIViewController {
   
+  let skView = SKView()
+  
+  var isPresenting = false
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    view.backgroundColor = UIColor.orange
+    
+    skView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(skView)
+    skView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+    skView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    skView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    skView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    if skView.bounds.width > 0 && skView.bounds.height > 0 && !isPresenting {
+      presentGameScene()
+      isPresenting = true
+    }
+  }
+  
+  func presentGameScene() {
     if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-      // Configure the view.
-      let skView = view as! SKView
       skView.showsFPS = true
       skView.showsNodeCount = true
-      
-      /* Sprite Kit applies additional optimizations to improve rendering performance */
       skView.ignoresSiblingOrder = true
-      
-      /* Set the scale mode to scale to fit the window */
       scene.scaleMode = .aspectFill
-      
       skView.presentScene(scene)
     }
   }
   
-  override var shouldAutorotate : Bool {
-    return true
-  }
-  
-  override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
-    if UIDevice.current.userInterfaceIdiom == .phone {
-      return UIInterfaceOrientationMask.allButUpsideDown
-    } else {
-      return UIInterfaceOrientationMask.all
-    }
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Release any cached data, images, etc that aren't in use.
-  }
-  
-  override var prefersStatusBarHidden : Bool {
-    return true
-  }
 }
